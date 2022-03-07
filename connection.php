0dc7cc21a7fdcbf8226e29fb77fe1afd245c081b
Earl Dahildahil <101031821+eodahildahil@users.noreply.github.com>
2022-03-07 15:19:30 +0800

    Update database credentials

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
 $db_host = "localhost";
-$db_username = "root";
-$db_password = "123qwe";
-$db_name = "assignment2";
+$db_username = "id18573451_root";
+$db_password = "DN5^X3v]KrNr]u9H";
+$db_name = "id18573451_database";
 
 if(!$connection = mysqli_connect($db_host, $db_username, $db_password, $db_name))
 {
